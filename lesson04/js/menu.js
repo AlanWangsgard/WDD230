9395a909dc07a4e9366733b8525414a90e799870
AlanWangsgard <alanwangsgard@byui.edu>
2021-05-14 13:03:22 -0600

    updates

--- lesson04/js/menu.js
+++ lesson04/js/menu.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
 }
+
+function date() {
+    var d = new Date()
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var full_date = day[d.getDay()] + ", " + " " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear()
+    var year = new Date().getFullYear();
+    var copy = document.querySelector(".copyright").innerHTML;
+    document.querySelector(".copyright").innerHTML = "© " + year + copy;
+    document.querySelector(".date").innerHTML = full_date;
+}
